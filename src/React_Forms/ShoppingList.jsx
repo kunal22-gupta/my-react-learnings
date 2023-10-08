33c84f75155f8019f871bbe3ef536118c0ae0252
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-09 01:23:12 +0530

    add functionality to remove item when clicked

--- src/React_Forms/ShoppingList.jsx
+++ src/React_Forms/ShoppingList.jsx
@@ -1,32 +1,33 @@
-import "./ShoppingList.css"
+import "./ShoppingList.css";
 
 import ShortUniqueId from "short-unique-id";
-import ShoppingListForm from "./ShoppingListForm"
-import { useState } from "react"
+import ShoppingListForm from "./ShoppingListForm";
+import { useState } from "react";
 
 export default function ShoppingList() {
-
-    const uid = new ShortUniqueId()
+    const uid = new ShortUniqueId();
     const [itemsList, setItemsList] = useState([
-        {id: uid.rnd(), product: "Apples", qty: 5},
-        {id: uid.rnd(), product: "Bananas", qty: 4}
-    ])
-    // const addItem = (item) => {
-    //     setItems((itemsList) => {
-    //         return [...itemsList, {...item}]
-    //     })
-    // }
+        { id: uid.rnd(), product: "Apples", qty: 5 },
+        { id: uid.rnd(), product: "Bananas", qty: 4 },
+    ]);
+    const removeItem = (e) => {
+        e.target.localName === "span"
+            ? e.target.parentNode.remove()
+            : e.target.remove();
+    };
     return (
         <div className="ShoppingList">
             <h1>Shopping List</h1>
             <ul>
-                {
-                    itemsList.map((item) => {
-                        return (<li key={item.id}><span>&#x1f4cc; {item.product}</span>  <span>x{item.qty}</span></li>)
-                    })
-                }
+                {itemsList.map((item) => {
+                    return (
+                        <li key={item.id} onClick={removeItem}>
+                            <span>{item.product}</span> <span>x{item.qty}</span>
+                        </li>
+                    );
+                })}
             </ul>
-            <ShoppingListForm setItemsList={setItemsList} uid={uid}/>
+            <ShoppingListForm setItemsList={setItemsList} uid={uid} />
         </div>
-    )
+    );
 }
