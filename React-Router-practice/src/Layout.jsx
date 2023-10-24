d019c32e9c26b58290c3181e70ce2e7fca5f65cf
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-24 11:59:57 +0530

    assemble all components using React router

---
+++ React-Router-practice/src/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+export default function Layout() {
+    return (
+        <>
+            <Header/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+}
