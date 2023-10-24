6ed14916f5af13d9d346c2e3af40787f3df7f71f
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-24 11:01:34 +0530

    create a new directory for React router practice

---
+++ React-Router-practice/README.md
@@ -0,0 +1,3 @@
+# React Router practice
+
+In this code I practiced routing in react application usign react router.
---
+++ React-Router-practice/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+function App() {
+    return <>
+    <h1 className="text-3xl font-bold">React Router</h1>
+    </>;
+}
+
+export default App;
